5076be98cb5a61b1ddffa807e938937c126a7629
nicoschonfeld <yznicolass@gmail.com>
2022-07-27 11:30:42 -0300

    Funcionalidad en la tabla de la página home

--- src/components/Alta/wizard/steps/LoteManjeo.jsx
+++ src/components/Alta/wizard/steps/LoteManjeo.jsx
@@ -1,57 +1,255 @@
-// ** React Imports
-import { Fragment } from 'react'
+//** import hooks from react
+import { useContext } from "react";
 
-// ** Icons Imports
-import { ArrowLeft, ArrowRight } from 'react-feather'
-import { Link } from 'react-router-dom'
+// ** Import DataContext from LoteProvider
+import { DataContext } from "../../../../utility/context/LoteProvider";
 
 // ** Reactstrap Imports
-import { Label, Row, Col, Input, Form, Button, FormGroup } from 'reactstrap'
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardBody,
+  Row,
+  Col,
+  Input,
+  Form,
+  Button,
+  Label,
+} from "reactstrap";
 
-const LoteManjeo = ({ stepper }) => {
+const LoteManjeo = () => {
+  // ** useContext
+  const {
+    data,
+    setData,
+    deleteTask,
+    handleSubmit,
+    nameLote,
+    setNameLote,
+    fieldLote,
+    setFieldLote,
+    locationLote,
+    setLocationLote,
+    cityLote,
+    setCityLote,
+    dateLote,
+    setDateLote,
+    cropLote,
+    setCropLote,
+    provinceLote,
+    setProvinceLote,
+  } = useContext(DataContext);
+
   return (
-    <Fragment>
-      <div className='content-header'>
-        <h3 className='mb-0'>Lote y Manjeo (Modificar página jira)</h3>
-      </div>
-      <Form onSubmit={e => e.preventDefault()}>
-        <FormGroup >
-        <Row className='match-height mt-1'>
-            <Col md='6' sm='12' >
-              <Label className='mt-1'>Nombre</Label>
-              <Input placeholder='Nombre' />
-              <Label className='mt-1'>Localidad</Label>
-              <Input placeholder='Localidad' />
-              <Label className='mt-1'>Tiempo transcurrido entre cosecha y siembra</Label>
-              <Input placeholder='Tiempo' />
-              <Label className='mt-1'>Rendimiento del cultivo antecesor en qq/ha</Label>
-              <Input placeholder='Rendimiento del cultivo antecesor en qq/ha' />
-            </Col>
-            <Col md='6' sm='12'>
-              <Label className='mt-1'>Campo o establecimiento</Label>
-              <Input placeholder='establecimiento' />
-              <Label className='mt-1'>Cultivo antecesor</Label>
-              <Input placeholder='Cultivo antecesor' />
-              <Label className='mt-1'>¿Se realiza siembra directa o lote arado?</Label>
-              <Input placeholder='¿Se realiza siembra directa o lote arado?' />
-              <Label className='mt-1'>Cantidad de hectáreas</Label>
-              <Input placeholder='Cantidad de hectáreas' />
-            </Col>
-        </Row>
-        </FormGroup>
-        <div className='d-flex justify-content-end'>
-          {/* <Button color='secondary' className='btn-prev' outline disabled>
-            <ArrowLeft size={14} className='align-middle me-sm-25 me-0'></ArrowLeft>
-            <span className='align-middle d-sm-inline-block d-none'>Atras</span>
-          </Button> */}
-          <Link to="/home" className="btn btn-primary">
-            <span className='align-middle d-sm-inline-block d-none'>Guardar</span>
-            {/* <ArrowRight size={14} className='align-middle ms-sm-25 ms-0'></ArrowRight> */}
-          </Link>
-        </div>
-      </Form>
-    </Fragment>
-  )
-}
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle tag="h4">Lote y Manejo</CardTitle>
+        </CardHeader>
 
-export default LoteManjeo
+        <CardBody>
+          <Form onSubmit={handleSubmit}>
+            <Row>
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label" for="nameMulti">
+                  Nombre
+                </Label>
+                <Input
+                  type="text"
+                  name="name"
+                  id="nameMulti"
+                  placeholder="Nombre"
+                  value={nameLote}
+                  onChange={(e) => setNameLote(e.target.value)}
+                />
+              </Col>
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label" for="lastNameMulti">
+                  Campo o Establecimiento
+                </Label>
+                <Input
+                  type="text"
+                  name="lastname"
+                  id="lastNameMulti"
+                  placeholder="Campo o Establecimiento"
+                  value={fieldLote}
+                  onChange={(e) => setFieldLote(e.target.value)}
+                />
+              </Col>
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label" for="cityMulti">
+                  Cantidad de Hectárias
+                </Label>
+                <Input
+                  type="text"
+                  name="city"
+                  id="cityMulti"
+                  placeholder="Cantidad de Hectárias"
+                />
+              </Col>
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label" for="CountryMulti">
+                  Localidad
+                </Label>
+                <Input
+                  type="text"
+                  name="country"
+                  id="CountryMulti"
+                  placeholder="Localidad"
+                  value={locationLote}
+                  onChange={(e) => setLocationLote(e.target.value)}
+                />
+              </Col>
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label" for="CountryMulti">
+                  Provincia
+                </Label>
+                <Input
+                  type="text"
+                  name="country"
+                  id="CountryMulti"
+                  placeholder="Localidad"
+                  value={provinceLote}
+                  onChange={(e) => setProvinceLote(e.target.value)}
+                />
+              </Col>
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label" for="CountryMulti">
+                  Pais
+                </Label>
+                <Input
+                  type="text"
+                  name="country"
+                  id="CountryMulti"
+                  placeholder="Localidad"
+                />
+              </Col>
+
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label me-2 fs-5" for="CountryMulti">
+                  Realiza siembra directa
+                </Label>
+                <Input
+                  type="checkbox"
+                  name="country"
+                  id="CountryMulti"
+                  placeholder="Realiza siembra directa"
+                />
+              </Col>
+
+              <span className="mb-5"></span>
+
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label" for="CompanyMulti">
+                  Cultivo antecesor
+                </Label>
+                <Input
+                  type="text"
+                  name="company"
+                  id="CompanyMulti"
+                  placeholder="Cultivo antecesor"
+                />
+              </Col>
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label" for="EmailMulti">
+                  Rendimiento del cultivo antecesor en qq/ha
+                </Label>
+                <Input
+                  type="email"
+                  name="Email"
+                  id="EmailMulti"
+                  placeholder="Rendimiento del cultivo antecesor en qq/ha"
+                />
+              </Col>
+              <Col md="6" sm="12" className="mb-1">
+                <Label className="form-label" for="EmailMulti">
+                  Tiempo transcurrido entre cosecha y siembra
+                </Label>
+                <Input
+                  type="email"
+                  name="Email"
+                  id="EmailMulti"
+                  placeholder="Tiempo transcurrido entre cosecha y siembra"
+                />
+              </Col>
+              <Col sm="12">
+                <div className="d-flex align-items-center justify-content-end">
+                  <Button
+                    className="btn btn-primary"
+                    color="primary"
+                    type="submit"
+                  >
+                    Guardar
+                  </Button>
+                  {/* <Button outline color="secondary" type="reset">
+                  Reset
+                </Button> */}
+                </div>
+              </Col>
+            </Row>
+          </Form>
+        </CardBody>
+      </Card>
+    </>
+  );
+};
+export default LoteManjeo;
+
+// // ** React Imports
+// import { Fragment } from 'react'
+
+// // ** Icons Imports
+// import { ArrowLeft, ArrowRight } from 'react-feather'
+// import { Link } from 'react-router-dom'
+
+// // ** Reactstrap Imports
+// import { Label, Row, Col, Input, Form, Button, FormGroup } from 'reactstrap'
+
+// const LoteManjeo = ({ stepper }) => {
+//   return (
+//     <Fragment>
+//       <div className='content-header'>
+//         <h3 className='mb-0'>Lote y Manjeo (Modificar página jira)</h3>
+//       </div>
+//       <Form onSubmit={e => e.preventDefault()}>
+//         <FormGroup >
+//         <Row className='match-height mt-1'>
+//             <Col md='6' sm='12' >
+//               <Label className='mt-1'>Nombre</Label>
+//               <Input placeholder='Nombre' />
+//               <Label className='mt-1'>Localidad</Label>
+//               <Input placeholder='Localidad' />
+//               <Label className='mt-1'>Tiempo transcurrido entre cosecha y siembra</Label>
+//               <Input placeholder='Tiempo' />
+//               <Label className='mt-1'>Rendimiento del cultivo antecesor en qq/ha</Label>
+//               <Input placeholder='Rendimiento del cultivo antecesor en qq/ha' />
+//             </Col>
+//             <Col md='6' sm='12'>
+//               <Label className='mt-1'>Campo o establecimiento</Label>
+//               <Input placeholder='establecimiento' />
+//               <Label className='mt-1'>Cultivo antecesor</Label>
+//               <Input placeholder='Cultivo antecesor' />
+//               <Label className='mt-1'>¿Se realiza siembra directa o lote arado?</Label>
+//               <Input placeholder='¿Se realiza siembra directa o lote arado?' />
+//               <Label className='mt-1'>Cantidad de hectáreas</Label>
+//               <Input placeholder='Cantidad de hectáreas' />
+//             </Col>
+//         </Row>
+//         </FormGroup>
+//         <div className='d-flex justify-content-end'>
+//           {/* <Button color='secondary' className='btn-prev' outline disabled>
+//             <ArrowLeft size={14} className='align-middle me-sm-25 me-0'></ArrowLeft>
+//             <span className='align-middle d-sm-inline-block d-none'>Atras</span>
+//           </Button> */}
+//           <Link to="/home" className="btn btn-primary">
+//             <span className='align-middle d-sm-inline-block d-none'>Guardar</span>
+//             {/* <ArrowRight size={14} className='align-middle ms-sm-25 ms-0'></ArrowRight> */}
+//           </Link>
+//         </div>
+//       </Form>
+//     </Fragment>
+//   )
+// }
+
+// export default LoteManjeo
--- src/views/tables/TableCompany.jsx
+++ src/views/tables/TableCompany.jsx
@@ -1,3 +1,5 @@
+import React, { useContext, useState } from "react";
+
 // ** Icons Imports
 import {
   MoreVertical,
@@ -21,168 +23,224 @@ import {
   CardHeader,
   CardTitle,
   CardBody,
+  Modal,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
 } from "reactstrap";
 
 // ** Import Link from react-router-dom
 import { Link } from "react-router-dom";
 
+// ** import icon ToolBox
+import toolBox from "@src/assets/images/icons/toolbox.svg";
+
+// ** import dataContext from LoteProvider
+import { DataContext } from "../../utility/context/LoteProvider";
+
 const TableCompany = () => {
+  const { data, deleteTask } = useContext(DataContext);
+  const [verModal, setVerModal] = useState(false);
+
   const medidaDesktop = document.documentElement.clientWidth;
 
-  const data = [
-    {
-      img: require("@src/assets/images/icons/toolbox.svg").default,
-      name: "Lote 1",
-      // description: 'Alta productividad',
-      icon: <Monitor size={18} />,
-      countryside: "Gastaldi",
-      location: "Villa María",
-      city: "Córdoba",
-      date: "15/12/2022",
-      crop: "Maíz",
-      cropUp: true,
-      btnDelete: <Trash className="me-50" size={15} />,
-    },
-    {
-      img: require("@src/assets/images/icons/book.svg").default,
-      name: "Lote 5",
-      // description: null,
-      icon: <Monitor size={18} />,
-      countryside: "El Mimoso",
-      location: "Río IV",
-      city: "Córdoba",
-      date: "11/11/2022",
-      crop: "Soja",
-      cropUp: false,
-      btnDelete: <Trash className="me-50" size={15} />,
-    },
-    {
-      img: require("@src/assets/images/icons/book.svg").default,
-      name: "Lote 2",
-      // description: 'Baja productividad',
-      icon: <Monitor size={18} />,
-      countryside: "Aleman",
-      location: "Pasco",
-      city: "Córdoba",
-      date: null,
-      crop: null,
-      cropUp: null,
-      btnDelete: <Trash className="me-50" size={15} />,
-    },
-    {
-      img: require("@src/assets/images/icons/toolbox.svg").default,
-      name: "Lote 3",
-      // description: null,
-      icon: <Monitor size={18} />,
-      countryside: "Antonio",
-      location: "Esperanza",
-      city: "Santa Fe",
-      date: "01/04/2022",
-      crop: "Alfalfa",
-      cropUp: true,
-      btnDelete: <Trash className="me-50" size={15} />,
-    },
-  ];
+  // const data = [
+  //   {
+  //     // img: require("@src/assets/images/icons/toolbox.svg").default,
+  //     name: "+ Lote",
+  //     // description: 'Alta productividad',
+  //     // icon: <Monitor size={18} />,
+  //     countryside: null,
+  //     location: null,
+  //     city: null,
+  //     date: null,
+  //     crop: null,
+  //     cropUp: null,
+  //     // btnDelete: <Trash className="me-50" size={15} />,
+  //   },
+  // {
+  //   // img: require("@src/assets/images/icons/toolbox.svg").default,
+  //   name: "Lote 1",
+  //   // description: 'Alta productividad',
+  //   // icon: <Monitor size={18} />,
+  //   countryside: "Gastaldi",
+  //   location: "Villa María",
+  //   city: "Córdoba",
+  //   date: "15/12/2022",
+  //   crop: "Maíz",
+  //   cropUp: true,
+  //   // btnDelete: <Trash className="me-50" size={15} />,
+  // },
+  // {
+  //   img: require("@src/assets/images/icons/book.svg").default,
+  //   name: "Lote 5",
+  //   // description: null,
+  //   icon: <Monitor size={18} />,
+  //   countryside: "El Mimoso",
+  //   location: "Río IV",
+  //   city: "Córdoba",
+  //   date: "11/11/2022",
+  //   crop: "Soja",
+  //   cropUp: false,
+  //   btnDelete: <Trash className="me-50" size={15} />,
+  // },
+  // {
+  //   // img: require("@src/assets/images/icons/book.svg").default,
+  //   name: "Lote 2",
+  //   // description: 'Baja productividad',
+  //   // icon: <Monitor size={18} />,
+  //   countryside: "Aleman",
+  //   location: "Pasco",
+  //   city: "Córdoba",
+  //   date: null,
+  //   crop: null,
+  //   cropUp: null,
+  //   // btnDelete: <Trash className="me-50" size={15} />,
+  // },
+  // {
+  //   img: require("@src/assets/images/icons/toolbox.svg").default,
+  //   name: "Lote 3",
+  //   // description: null,
+  //   icon: <Monitor size={18} />,
+  //   countryside: "Antonio",
+  //   location: "Esperanza",
+  //   city: "Santa Fe",
+  //   date: "01/04/2022",
+  //   crop: "Alfalfa",
+  //   cropUp: true,
+  //   btnDelete: <Trash className="me-50" size={15} />,
+  // },
+  // ];
 
   const renderData = () => {
     return (
       <>
         {data.map((col, index) => (
-          <tr key={index}>
-            {/* NOMBRE */}
-            <td>
-              <div className="d-flex aling-items-center">
-                <img className="me-75" src={col.img} alt={col.name} />
+          <>
+            <tr key={index}>
+              {/* NOMBRE */}
+              <td>
+                <div className="d-flex aling-items-center">
+                  <img className="me-75" src={toolBox} alt={col.name} />
+                  <div className="d-flex flex-column">
+                    <Link to="/new-lote" className="align-middle fw-bolder">
+                      {col.name}
+                    </Link>
+                    <span className="font-small-2 text-muted">
+                      {col.description}
+                    </span>
+                  </div>
+                </div>
+              </td>
+              {/* CAMPO */}
+              <td>
+                <span className="fw-bolder mb-25">{col.field}</span>
+              </td>
+              {/* LOCALIDAD */}
+              <td className="text-nowrap">
                 <div className="d-flex flex-column">
-                  <Link to="/new-lote" className="align-middle fw-bolder">
-                    {col.name}
-                  </Link>
+                  <span className="fw-bolder mb-25">{col.location}</span>
                   <span className="font-small-2 text-muted">
-                    {col.description}
+                    {col.province}
                   </span>
                 </div>
-              </div>
-            </td>
-            {/* CAMPO */}
-            <td>
-              <span className="fw-bolder mb-25">{col.countryside}</span>
-            </td>
-            {/* LOCALIDAD */}
-            <td className="text-nowrap">
-              <div className="d-flex flex-column">
-                <span className="fw-bolder mb-25">{col.location}</span>
-                <span className="font-small-2 text-muted">{col.city}</span>
-              </div>
-            </td>
-            {/* CULTIVO */}
-            <td>
-              <div className="d-flex align-items-center">
-                <span className="fw-bolder ms-1">{col.crop}</span>
-                {/* {col.cropUp ? (<TrendingUp size={15} className="text-success" />) : (<TrendingDown size={15} className="text-danger" />)} */}
-              </div>
-            </td>
-            {/* FECHA */}
-            <td>
-              <Badge
-                pill
-                color={col.cropUp ? "light-primary" : "light-danger"}
-                className="ms-1"
-              >
-                {col.date}
-              </Badge>
-            </td>
-            <td>
-              <UncontrolledDropdown className="ms-4">
-                <DropdownToggle
-                  className="icon-btn hide-arrow"
-                  color="transparent"
-                  size="sm"
-                  caret
+              </td>
+              {/* CULTIVO */}
+              <td>
+                <div className="d-flex align-items-center">
+                  <span className="fw-bolder ms-1">{col.crop}</span>
+                  {/* {col.cropUp ? (<TrendingUp size={15} className="text-success" />) : (<TrendingDown size={15} className="text-danger" />)} */}
+                </div>
+              </td>
+              {/* FECHA */}
+              <td>
+                <Badge
+                  pill
+                  color={col.cropUp ? "light-primary" : "light-danger"}
+                  className="ms-1"
                 >
-                  <MoreVertical size={15} />
-                </DropdownToggle>
+                  {col.date}
+                </Badge>
+              </td>
+              <td>
+                <UncontrolledDropdown className="ms-4">
+                  <DropdownToggle
+                    className="icon-btn hide-arrow"
+                    color="transparent"
+                    size="sm"
+                    caret
+                  >
+                    <MoreVertical size={15} />
+                  </DropdownToggle>
 
-                <DropdownMenu>
-                  <DropdownItem href="/" onClick={(e) => e.preventDefault()}>
-                    {col.crop ? (
-                      <Link to="/rendimiento">
-                        <Monitor className="me-50" size={15} />{" "}
-                        <span className="align-middle">Ver</span>
+                  <DropdownMenu>
+                    <DropdownItem className="w-100">
+                      {col.crop && col.date ? (
+                        <Link to="/rendimiento">
+                          <Monitor className="me-50" size={15} />{" "}
+                          <span className="align-middle">Ver</span>
+                        </Link>
+                      ) : (
+                        <span onClick={() => setVerModal(!verModal)}>
+                          <Monitor className="me-50" size={15} />{" "}
+                          <span className="align-middle">Ver</span>
+                        </span>
+                      )}
+                    </DropdownItem>
+
+                    <DropdownItem className="w-100">
+                      <Link to="/Alta">
+                        <Edit className="me-50" size={15} />{" "}
+                        <span className="align-middle">Agregar / Editar</span>
                       </Link>
-                    ) : (
-                      <span
-                        onClick={() =>
-                          alert("Se necesita añadir recomendación")
-                        }
-                      >
-                        <Monitor className="me-50" size={15} />{" "}
-                        <span className="align-middle">Ver</span>
-                      </span>
-                    )}
-                  </DropdownItem>
+                    </DropdownItem>
 
-                  <DropdownItem href="/" onClick={(e) => e.preventDefault()}>
-                    <Link to="/Alta">
-                      <Edit className="me-50" size={15} />{" "}
-                      <span className="align-middle">Agregar / Editar</span>
-                    </Link>
-                  </DropdownItem>
-
-                  {/* <DropdownItem href='/' onClick={e => e.preventDefault()}>
+                    {/* <DropdownItem href='/' onClick={e => e.preventDefault()}>
                 <Trash className='me-50' size={15} /> <span className='align-middle'>Eliminar</span>
                 </DropdownItem> */}
-                </DropdownMenu>
-              </UncontrolledDropdown>
-            </td>
-            <td>
-              <span
-                className="hoverDelete"
-                onClick={() => alert("Se elimina el lote")}
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+              </td>
+              <Modal
+                isOpen={verModal}
+                // toggle={() => setCenteredModal(!centeredModal)}
+                className="modal-dialog-centered"
               >
-                {col.btnDelete}
-              </span>
-            </td>
-          </tr>
+                {/* toggle={() => setCenteredModal(!centeredModal)} */}
+                <ModalHeader>Atención</ModalHeader>
+                <ModalBody>
+                  <h4 className="mb-2">
+                    Para poder ver los resultados de su lote, necesita agregar
+                    una recomendación.
+                  </h4>
+                  <span>
+                    Puede crear una recomendación a traves de la tabla en la
+                    columna{" "}
+                    <span className="color-ejemplo fw-bold">
+                      MIS RECOMENDACIONES
+                    </span>
+                    .
+                  </span>
+                </ModalBody>
+                <ModalFooter>
+                  <Button
+                    color="primary"
+                    onClick={() => setVerModal(!verModal)}
+                  >
+                    Aceptar
+                  </Button>
+                </ModalFooter>
+              </Modal>
+              <td>
+                <span
+                  className="hoverDelete"
+                  onClick={() => deleteTask(col.id)}
+                >
+                  <Trash className="me-50" size={15} />
+                </span>
+              </td>
+            </tr>
+          </>
         ))}
       </>
     );
@@ -192,24 +250,64 @@ const TableCompany = () => {
     <>
       <CardHeader className="d-flex align-items-center justify-content-between">
         <CardTitle>Mis Lotes</CardTitle>
-        {/* <Link to="/new-lote" className="btn btn-primary">+ Lote</Link> */}
+        {data.length === 0 && (
+          <Link to="/new-lote" className="btn btn-primary">
+            + Lote
+          </Link>
+        )}
       </CardHeader>
 
       <CardBody>
-        <Table responsive hover>
-          <thead>
-            <tr>
-              <th>NOMBRE</th>
-              <th>CAMPO</th>
-              <th>LOCALIDAD</th>
-              <th>CULTIVO</th>
-              <th>FECHA DE SIEMBRA</th>
-              <th>MIS RECOMENDACIONES</th>
-              <th> </th>
-            </tr>
-          </thead>
-          <tbody>{renderData()}</tbody>
-        </Table>
+        {data.length === 1 ? (
+          <>
+            {medidaDesktop <= 1024 ? (
+              <Table responsive hover>
+                <thead>
+                  <tr>
+                    <th>NOMBRE</th>
+                    <th>CAMPO</th>
+                    <th>LOCALIDAD</th>
+                    <th>CULTIVO</th>
+                    <th>FECHA DE SIEMBRA</th>
+                    <th>MIS RECOMENDACIONES</th>
+                    <th> </th>
+                  </tr>
+                </thead>
+                <tbody>{renderData()}</tbody>
+              </Table>
+            ) : (
+              <Table hover>
+                <thead>
+                  <tr>
+                    <th>NOMBRE</th>
+                    <th>CAMPO</th>
+                    <th>LOCALIDAD</th>
+                    <th>CULTIVO</th>
+                    <th>FECHA DE SIEMBRA</th>
+                    <th>MIS RECOMENDACIONES</th>
+                    <th> </th>
+                  </tr>
+                </thead>
+                <tbody>{renderData()}</tbody>
+              </Table>
+            )}
+          </>
+        ) : (
+          <Table responsive hover>
+            <thead>
+              <tr>
+                <th>NOMBRE</th>
+                <th>CAMPO</th>
+                <th>LOCALIDAD</th>
+                <th>CULTIVO</th>
+                <th>FECHA DE SIEMBRA</th>
+                <th>MIS RECOMENDACIONES</th>
+                <th> </th>
+              </tr>
+            </thead>
+            <tbody>{renderData()}</tbody>
+          </Table>
+        )}
       </CardBody>
     </>
   );
